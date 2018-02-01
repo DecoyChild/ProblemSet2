a6e5f6464acbe90fe2e325b397d1f74ebba31829
DecoyChild <32802838+DecoyChild@users.noreply.github.com>
2018-02-01 18:32:45 -0500

    Add files via upload

---
+++ PS22.java
@@ -0,0 +1,18 @@
+/*
+* @author shayne
+* CIS 252
+* Project title
+* Date
+* 
+* Description of Program
+*/
+
+package ps2.pkg2;
+
+public class PS22 {
+
+    public static void main(String[] args) {
+        //Write some sweet ass CODE!!
+    }
+
+}
